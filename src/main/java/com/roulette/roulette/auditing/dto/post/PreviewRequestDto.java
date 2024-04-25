b6c07e18fda41662fb685582397aea132222e6af
dong3058 <hwangdonggeun70@gmail.com>
2024-04-25 19:09:04 +0900

    생성자 추가

--- src/main/java/com/roulette/roulette/auditing/dto/post/PreviewRequestDto.java
+++ src/main/java/com/roulette/roulette/auditing/dto/post/PreviewRequestDto.java
@@ -6,4 +6,9 @@ import lombok.*;
 @NoArgsConstructor
 public class PreviewRequestDto {
     private Long replyId;
+
+
+    public PreviewRequestDto(Long replyId) {
+        this.replyId = replyId;
+    }
 }
