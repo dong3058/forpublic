6b775a8a644ca1a9b188fd06259eca067bf82242
dong3058 <hwangdonggeun70@gmail.com>
2024-04-25 21:56:10 +0900

    first

--- src/main/java/com/roulette/roulette/entity/PostImage.java
+++ src/main/java/com/roulette/roulette/entity/PostImage.java
@@ -11,7 +11,7 @@ public class PostImage {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_img_id")
     private Long postImgId;
-
+//
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
